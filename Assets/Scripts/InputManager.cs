627230f24b00bf5fc27f925c6ffbee7e1b4d46ff
Miguel Pacheco <miguel95_angel@hotmail.com>
2023-11-15 20:54:01 -0400

    Player Movement New Unity Input System

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager _instance;
+    public static InputManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Input Manger Instance is NULL");
+            }
+
+            return _instance;
+        }
+
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    PlayerInputActions _input;
+
+    private void Start()
+    {
+        _input = new PlayerInputActions();
+        _input.Player.Enable();
+    }
+
+    public void MovePlayer(Transform player, float speed)
+    {
+        var move = _input.Player.Movement.ReadValue<Vector2>();
+        player.Translate(new Vector3(move.x, 0, move.y) * Time.deltaTime * speed);
+    }
+}
